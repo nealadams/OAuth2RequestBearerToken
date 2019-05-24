557d1b47c422edbf291774dddf6b34953a77fa81
nealadams <nealadams1@gmail.com>
2019-05-23 22:32:26 -0500

    Removed using statements that were not necessary.

--- OAuth2RequestBearerToken/Program.cs
+++ OAuth2RequestBearerToken/Program.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Net;
-using System.Net.Http;
 using Microsoft.Extensions.Configuration;
-using Newtonsoft.Json;
 
 namespace OAuth2RequestBearerToken
 {
