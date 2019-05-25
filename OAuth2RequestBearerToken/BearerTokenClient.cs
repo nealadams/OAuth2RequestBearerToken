c805cd2dfabeaa53828b602928f2af75ff0ef8bd
nealadams <nealadams1@gmail.com>
2019-05-24 23:21:16 -0500

    Performance improvements.

--- OAuth2RequestBearerToken/BearerTokenClient.cs
+++ OAuth2RequestBearerToken/BearerTokenClient.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections.Generic;
-using System.Net;
+using System.IO;
 using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
@@ -12,27 +15,57 @@ namespace OAuth2RequestBearerToken
 
         public async Task<OAuth2TokenResponse> GetBearerToken(OAuth2Config config)
         {
-            List<KeyValuePair<string, string>> headers = new List<KeyValuePair<string, string>>();
-            headers.Add(new KeyValuePair<string, string>("grant_type", "client_credentials"));
-            headers.Add(new KeyValuePair<string, string>("client_id", config.client_id));
-            headers.Add(new KeyValuePair<string, string>("client_secret", config.client_secret));
-            headers.Add(new KeyValuePair<string, string>("resource", config.resource));
-            
             _httpClient = new HttpClient();
+            IEnumerable<KeyValuePair<string, string>> headers = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("grant_type", "client_credentials"),
+                new KeyValuePair<string, string>("client_id", config.client_id),
+                new KeyValuePair<string, string>("client_secret", config.client_secret),
+                new KeyValuePair<string, string>("resource", config.resource)
+            };
 
             HttpContent content = new FormUrlEncodedContent(headers);
+            CancellationToken cancellationToken = new CancellationToken();
             
-            var response = _httpClient.PostAsync(config.url, content);
+            HttpRequestMessage requestMessage = new HttpRequestMessage();
+            requestMessage.Content = content;
+            requestMessage.Method = HttpMethod.Post;
+            requestMessage.RequestUri = new Uri(config.url);
+            
+            var response = _httpClient.SendAsync(requestMessage, HttpCompletionOption.ResponseHeadersRead, cancellationToken);
 
-            if (response.Result.StatusCode == HttpStatusCode.OK)
+            var stream = await response.Result.Content.ReadAsStreamAsync();
+            if (response.Result.IsSuccessStatusCode)
+                return DeserializeJsonFromStream<OAuth2TokenResponse>(stream);
+            
+            // Throw exception if deserialization fails.
+            var streamContent = await StreamToStringAsync(stream);
+            throw new SerializationException(streamContent);
+        }
+        
+        private static T DeserializeJsonFromStream<T>(Stream stream)
+        {
+            if (stream == null || stream.CanRead == false)
+                return default;
+
+            using (var streamReader = new StreamReader(stream))
+            using (var jsonTextReader = new JsonTextReader(streamReader))
             {
-                var responseContentAsString = await response.Result.Content.ReadAsStringAsync();
-                var ob = JsonConvert.DeserializeObject<OAuth2TokenResponse>(responseContentAsString);
-                return ob;
+                var jsonSerializer = new JsonSerializer();
+                var searchResult = jsonSerializer.Deserialize<T>(jsonTextReader);
+                return searchResult;
             }
+        }
+        
+        private static async Task<string> StreamToStringAsync(Stream stream)
+        {
+            string content = null;
 
-            return new OAuth2TokenResponse();
+            if (stream != null)
+                using (var streamReader = new StreamReader(stream))
+                    content = await streamReader.ReadToEndAsync();
 
+            return content;
         }
     }
 }
